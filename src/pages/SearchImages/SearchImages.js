24c3d5f66311fd768abf92f424571349151d84a2
Julie Yudowitch <JulieYudo@gmail.com>
2021-12-02 05:54:42 -0500

    addedthumbnail image for sharing

--- src/pages/SearchImages/SearchImages.js
+++ src/pages/SearchImages/SearchImages.js
@@ -5,9 +5,10 @@ function SearchImages() {
     return (
         <div>
           <h2 className='promise'>Coming Soon!</h2>
-          <p>Image Search</p>
-          <div className='photos'>
-             <PhotoSearch />
+        <p>Image Search</p>
+        <a href="https://ibb.co/dDBB1JM"><img src="https://i.ibb.co/5LTTtx4/james-harrison-vp-Oe-Xr5wm-R4-unsplash.jpg" alt="james-harrison-vp-Oe-Xr5wm-R4-unsplash" border="0"/></a>
+        <div className='photos'>
+          <PhotoSearch />
           </div>
         </div>
     )
