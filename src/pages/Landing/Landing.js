1d23a05fab8d5bcf74e07bab3f9e32545fda1e40
Julie Yudowitch <JulieYudo@gmail.com>
2021-12-03 09:23:31 -0500

    changes to header and nav

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -4,7 +4,7 @@ function Landing() {
     return (
         <div className='plans'>
             <h2 className='plans-title'>Plans for this App</h2>
-            <p>Finish styling and mobile version (I am actively working on this part so if it looks bad try back a bit later -12/3/2021)</p>
+            <p>Finish styling and mobile version (I am actively working on this part -12/3/2021)</p>
             <p>Add functionality to existing pages</p>
             <p>User Login</p>
             <p>User settings including dark mode</p>
