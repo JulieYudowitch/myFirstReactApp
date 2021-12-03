e4d795cb7c166a104d679ceacb3f8a3616326730
Julie Yudowitch <JulieYudo@gmail.com>
2021-12-03 12:41:31 -0500

    improved mobile nav

--- src/components/Nav/MobileNav.js
+++ src/components/Nav/MobileNav.js
@@ -1,7 +1,7 @@
 import './Nav.css';
-import NavLinks from './NavLinks';
 import { GrMenu } from 'react-icons/gr';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function MobileNav() {
     const [open, setOpen] = useState(false);
@@ -9,7 +9,12 @@ function MobileNav() {
     return (
         <nav className='mobilenav'>
           <GrMenu className='menu' size='40px' color='white' onClick={() => setOpen(!open)}/>
-          {open && <NavLinks />}
+            {open &&
+                <ul className='navlinks'>
+                  <Link to="/SearchImages" className="item" onClick={() => setOpen(!open)}>Search Images</Link>
+                  <Link to="/Blog" className="item" onClick={() => setOpen(!open)}>Blog & Comments</Link>
+                  <Link to="/Shop" className="item" onClick={() => setOpen(!open)}>Shop</Link>
+                </ul>}
         </nav>
     )
 }
--- src/components/Nav/NavBar.js
+++ src/components/Nav/NavBar.js
@@ -1,10 +1,18 @@
 import './Nav.css';
 import NavLinks from './NavLinks';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function NavBar() {
+    const [open, setOpen] = useState(false);
+
     return (
         <nav className='navbar'>
-          <NavLinks />
+          <ul className='navlinks'>
+                  <Link to="/SearchImages" className="item" onClick={() => setOpen(!open)}>Search Images</Link>
+                  <Link to="/Blog" className="item" onClick={() => setOpen(!open)}>Blog & Comments</Link>
+                  <Link to="/Shop" className="item" onClick={() => setOpen(!open)}>Shop</Link>
+                </ul>
         </nav>
     )
 }
