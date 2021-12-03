d69aaf17aea9a502302f94776c6cfd1bae76bf08
Julie Yudowitch <JulieYudo@gmail.com>
2021-12-03 12:59:50 -0500

    centered items on search page

--- src/pages/SearchImages/SearchImages.js
+++ src/pages/SearchImages/SearchImages.js
@@ -1,13 +1,12 @@
 import './SearchImages.css';
 import PhotoSearch from '../../components/PhotoSearch/PhotoSearch';
-
 function SearchImages() {
     return (
         <div>
           <h2 className='promise'>Coming Soon!</h2>
-          <p>Image Search</p>
-          <a className='placeholderImage' href="https://ibb.co/dDBB1JM"><img src="https://i.ibb.co/5LTTtx4/james-harrison-vp-Oe-Xr5wm-R4-unsplash.jpg" alt="james-harrison-vp-Oe-Xr5wm-R4-unsplash" border="0"/></a>
+          <p>Image Search</p>          
           <div className='photo-results'>
+            <a className='placeholderImage' href="https://ibb.co/dDBB1JM"><img src="https://i.ibb.co/5LTTtx4/james-harrison-vp-Oe-Xr5wm-R4-unsplash.jpg" alt="james-harrison-vp-Oe-Xr5wm-R4-unsplash" border="0" /></a>
             <PhotoSearch />
           </div>
         </div>
