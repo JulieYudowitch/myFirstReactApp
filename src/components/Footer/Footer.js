d3fd21b607e32d0250501e73f195626868a9f452
Julie Yudowitch <JulieYudo@gmail.com>
2021-12-03 10:55:39 -0500

    edited footer info

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
                       <p>JulieYudo@gmail.com</p>
                     </div>
                     <div>
-                      <p>some other info</p>
+                      
                     </div>
                   </div>
                   <p classsName='copyright'>&copy;{new Date().getFullYear()} Julie Yudowitch | All rights reserved</p>
