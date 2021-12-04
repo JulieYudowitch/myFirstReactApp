7e46027f902ac67c19a390bcc5a53dc323dbfa46
Julie Yudowitch <JulieYudo@gmail.com>
2021-12-04 08:22:48 -0500

    another attempt to fix thumbnail

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -2,14 +2,19 @@ import './Landing.css';
 
 function Landing() {
     return (
-        <div className='plans'>
-            <h2 className='plans-title'>Plans for this App</h2>
-            <p>Finish styling and mobile version (I am actively working on this part -12/3/2021)</p>
-            <p>Add functionality to existing pages</p>
-            <p>User Login</p>
-            <p>User settings including dark mode</p>
-            <p>More pages</p>
-            <p>& more!</p>
+        <div>
+            <div className='land-image-container'>
+                <div className='landing-image'></div>
+            </div>
+            <div className='plans'>
+                <h2 className='plans-title'>Plans for this App</h2>
+                <p>Finish styling and mobile version (I am actively working on this part -12/3/2021)</p>
+                <p>Add functionality to existing pages</p>
+                <p>User Login</p>
+                <p>User settings including dark mode</p>
+                <p>More pages</p>
+                <p>& more!</p>
+            </div>            
         </div>
               
     )
