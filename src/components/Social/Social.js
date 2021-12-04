2d8c108b099ac1b3cf61ff8456abee87e8d4b4e5
Julie Yudowitch <JulieYudo@gmail.com>
2021-12-04 16:47:23 -0500

    added target blank to social links

--- src/components/Social/Social.js
+++ src/components/Social/Social.js
@@ -17,39 +17,39 @@ function Social() {
     return (
         <div className='social'>
           <a href="https://www.linkedin.com/in/julie-yudowitch-041645208/"
-          className="linkedIn">
+          className="linkedIn" target='blank'>
           <FontAwesomeIcon icon={faLinkedin} size="2x" />
           </a>
           <a href="https://github.com/JulieYudowitch"
-          className="github">
+          className="github" target='blank'>
           <FontAwesomeIcon icon={faGithub} size="2x" />
           </a>
           <a href="https://julieyudowitch.medium.com/"
-          className="medium">
+          className="medium" target='blank'>
           <FontAwesomeIcon icon={faMedium} size="2x" />
           </a>
           <a href="https://julieyudowitch.blogspot.com/"
-          className="blogger">
+          className="blogger" target='blank'>
           <FontAwesomeIcon icon={faBlogger} size="2x" />
           </a>
           <a href="https://www.tiktok.com/@julieyudowitch?"
-          className="tiktok">
+          className="tiktok" target='blank'>
           <FontAwesomeIcon icon={faTiktok} size="2x" />
           </a>
           <a href="https://www.pinterest.com/julieyudowitch/_saved/"
-          className="pinterest">
+          className="pinterest" target='blank'>
           <FontAwesomeIcon icon={faPinterest} size="2x" />
           </a>
           <a href="https://www.facebook.com/Julieyudowitch"
-          className="facebook">
+          className="facebook" target='blank'>
           <FontAwesomeIcon icon={faFacebook} size="2x" />
           </a>
           <a href="https://twitter.com/YudowitchJulie"
-          className="twitter">
+          className="twitter" target='blank'>
           <FontAwesomeIcon icon={faTwitter} size="2x" />
           </a>
           <a href="https://www.instagram.com/julieyudowitch/"
-          className="instagram">
+          className="instagram" target='blank'>
           <FontAwesomeIcon icon={faInstagram} size="2x" />
           </a>
         </div>
