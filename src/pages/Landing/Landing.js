a29eacdbf8ae9b5a99a06dc4ee0609a8609cef15
Julie Yudowitch <JulieYudo@gmail.com>
2021-12-06 16:50:59 -0500

    mobile nav

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -3,7 +3,10 @@ import './Landing.css';
 function Landing() {
     return (
         <div className='landing-page'>
-            <div className='title'>                
+            <div className='title'>
+                <div>
+                   <img className='logo-on-title' src='https://i.ibb.co/jk5S5Tm/logotransp.png'/> 
+                </div>
                 <div className='description'>                                
                    <p>A work in progress</p>
                    <h1>By Julie Yudowitch</h1> 
