db33432d1bdf77329af45f3d53ef41dd552780cd
Julie Yudowitch <JulieYudo@gmail.com>
2021-12-06 17:14:48 -0500

    title

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -14,7 +14,9 @@ function Landing() {
             </div>
             <div className='plans'>
                 <h2 className='plans-title'>Plans for this App</h2>
-                <p>Finish styling (I am actively working on this part -12/3/2021)</p>
+                <div className='finished-task'>
+                    <p>Finish styling (I am actively working on this part -12/3/2021)</p>
+                </div>                
                 <p>Add functionality to existing pages</p>
                 <p>User Login</p>
                 <p>User settings including dark mode</p>
