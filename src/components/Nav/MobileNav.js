bedecea25ba7ffb9dc713637dada2a20b281bc57
Julie Yudowitch <JulieYudo@gmail.com>
2021-12-05 21:50:39 -0500

    almost fixed mobile nav

--- src/components/Nav/MobileNav.js
+++ src/components/Nav/MobileNav.js
@@ -7,29 +7,40 @@ function MobileNav() {
     const [open, setOpen] = useState(false);
 
     return (
-        <div className='mobile-nav-bar'>
-            {!open &&
-                <div className='mobile-nav'>
-                 <div>
-                  <Link to='/myFirstReactApp' className="nav-link"><img className='logo' src='https://i.ibb.co/t8xNLGC/logotransp.png'/></Link>
-                 </div>
-                </div>
-            }
+        <div>
             <div>
-                <div>
-                  <GrMenu className='burger-icon' size='40px' color='white' onClick={() => setOpen(!open)} />
-                </div>
+                {
+                    !open &&
+                      <div className='mobile-nav-bar'>
+                        <div>
+                          <Link to='/myFirstReactApp' className="nav-link"><img className='logo' src='https://i.ibb.co/t8xNLGC/logotransp.png'/></Link>
+                        </div>
+                        <div>
+                           <GrMenu className='burger-icon' size='40px' color='white' onClick={() => setOpen(!open)} />
+                        </div>
+                      </div>
+                }
             </div>
-            <div  className='mobile-menu'> 
-                         
-            {open &&
-                  <ul className='mobile-menu-ul'>
-                  <Link to="/SearchImages" className="mobile-nav-link" onClick={() => setOpen(!open)}>Search Images</Link>
-                  <Link to="/Blog" className="mobile-nav-link" onClick={() => setOpen(!open)}>Blog & Comments</Link>
-                  <Link to="/Shop" className="mobile-nav-link" onClick={() => setOpen(!open)}>Shop</Link>
-                </ul>}
+
+            <div className='mobile-menu'>
+                {
+                    open &&
+                      <div className='mobile-menu'>
+                        <ul className='mobile-menu-ul'>
+                          <Link to="/SearchImages" className="mobile-nav-link" onClick={() => setOpen(!open)}>Search Images</Link>
+                          <Link to="/Blog" className="mobile-nav-link" onClick={() => setOpen(!open)}>Blog & Comments</Link>
+                          <Link to="/Shop" className="mobile-nav-link" onClick={() => setOpen(!open)}>Shop</Link>
+                        </ul>
+                        <div>
+                          <GrMenu className='burger-icon' size='40px' color='white' onClick={() => setOpen(!open)} />
+                        </div>
+                      </div>
+                }
             </div>
         </div>
+
+
+
             
             
     )
