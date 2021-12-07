246bae02427a66538a92ed89153f0cc1682dadd6
Julie Yudowitch <JulieYudo@gmail.com>
2021-12-06 19:46:56 -0500

    added an alt text and noopener for open blanks

--- src/components/Nav/NavBar.js
+++ src/components/Nav/NavBar.js
@@ -8,10 +8,11 @@ function NavBar() {
     return (
       <div className='nav-bar'>
         <div className='logo'>
-          <Link to='/myFirstReactApp'><img className='logo' src='https://i.ibb.co/jk5S5Tm/logotransp.png'/></Link>
+          <Link to='/myFirstReactApp'><img className='logo' src='https://i.ibb.co/jk5S5Tm/logotransp.png' alt='Logo for My First React App'/></Link>
         </div>
         <div className='nav-links'>
           <ul className='nav-links'>
+            <Link to="/myFirstReactApp" className="nav-link" onClick={() => setOpen(!open)}>Home</Link>
             <Link to="/SearchImages" className="nav-link" onClick={() => setOpen(!open)}>Search Images</Link>
             <Link to="/Blog" className="nav-link" onClick={() => setOpen(!open)}>Blog & Comments</Link>
             <Link to="/Shop" className="nav-link" onClick={() => setOpen(!open)}>Shop</Link>
