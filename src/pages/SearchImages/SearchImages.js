1f1454b0dc30c8e8f08c99124dfeee11dd891573
Julie Yudowitch <JulieYudo@gmail.com>
2021-12-09 15:06:52 -0500

    image search

--- src/pages/SearchImages/SearchImages.js
+++ src/pages/SearchImages/SearchImages.js
@@ -4,7 +4,8 @@ import PhotoSearch from '../../components/PhotoSearch/PhotoSearch';
 function SearchImages() {
     return (
         <div className='image-search-page'>
-          <h1>Image Search</h1>        
+        <h1>Image Search</h1>
+        <p>Sometimes I like to just take a minute and look at pictures of cats.  What will you search for?</p>
           <div>          
             <PhotoSearch />
           </div>
