32619434e6271a09a7cfff11fca9bd64509b6d41
Julie Yudowitch <JulieYudo@gmail.com>
2021-12-11 11:23:34 -0500

    styles

--- src/pages/Shop/Shop.js
+++ src/pages/Shop/Shop.js
@@ -3,7 +3,7 @@ import './Shop.css';
 function Shop() {
     return (
         <div className='shop-page'>
-            <div className='shop-poster'>
+            <div className='shop-menu'>
             </div>
             
         </div>
