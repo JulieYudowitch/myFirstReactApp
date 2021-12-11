303e6009fa81099053191f8958f510aef2e5feff
Julie Yudowitch <JulieYudo@gmail.com>
2021-12-11 09:30:44 -0500

    style

--- src/pages/Shop/Shop.js
+++ src/pages/Shop/Shop.js
@@ -3,10 +3,7 @@ import './Shop.css';
 function Shop() {
     return (
         <div className='shop-page'>
-            <h1>Coming Soon!</h1>
-                <div className='shop-poster'>
-                <p className='shop-poster-info'>A store where you will actually be able to buy things!</p>
-                <p className='shop-poster-info'>Completely True Story...</p>
+            <div className='shop-poster'>
             </div>
             
         </div>
