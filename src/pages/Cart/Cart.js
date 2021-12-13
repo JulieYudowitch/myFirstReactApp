0e4f2ad4324f3c1d5e59c6f5ea5670b1bcdb8da0
Julie Yudowitch <JulieYudo@gmail.com>
2021-12-13 16:23:44 -0500

    style changes

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -3,7 +3,7 @@ import './Cart.css';
 function Cart() {
     return (
         <div className='cart-page'>
-          <h2>Your Cart is empty</h2>
+          <h2 className='empty-cart'>Your Cart is empty</h2>
         </div>
     )
 }
