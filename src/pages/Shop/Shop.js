1740322f50e1062b716f7066af74c853107024ac
Julie Yudowitch <JulieYudo@gmail.com>
2021-12-13 18:46:00 -0500

    shop

--- src/pages/Shop/Shop.js
+++ src/pages/Shop/Shop.js
@@ -4,9 +4,19 @@ function Shop() {
     return (
         <div className='shop-page'>
             <div className='shop-poster'>
-                <div className='shop-menu'>
-                    <h2 className='promise'>Coming Soon</h2>
-
+                <div className='shop-sidebar'>
+                    <ul className='shop-menu-categories'>
+                        <li>Women's</li>
+                        <li>Men's</li>
+                        <li>Children's</li>
+                        <li>Accessories</li>
+                        <li>Gifts</li>
+                        <li>Jewelry</li>
+                        <li>Home Decor</li>
+                    </ul>
+                </div>
+                <div className='products'>
+           
                 </div>
             </div>
             
