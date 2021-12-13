ef46562bb817b10ed451918ea35f29c41c803e72
Julie Yudowitch <JulieYudo@gmail.com>
2021-12-13 08:06:19 -0500

    styles

--- src/pages/PhotoSearch/PhotoSearch.js
+++ src/pages/PhotoSearch/PhotoSearch.js
@@ -71,7 +71,7 @@ function PhotoSearch() {
                 
                 <div className='image-search-prod'>
                     <div><p className='image-search-greeting'>Sometimes I like to just take a minute and look at pictures of cats.  What will you search for?</p></div>
-                    <div className='image-search-prod-cat'><img className='image-search-prod-cat' src="https://i.ibb.co/mb5hj1X/catcroppedvert.jpg" alt="cat" border="0"/></div>
+                    <div className='image-search-prod-cat'><p>.</p></div>
                 </div>
                 
             }
@@ -87,3 +87,5 @@ function PhotoSearch() {
 }
 
 export default PhotoSearch;
+
+/*<img className='image-search-prod-cat' src="https://i.ibb.co/mb5hj1X/catcroppedvert.jpg" alt="cat" border="0"/>*/
