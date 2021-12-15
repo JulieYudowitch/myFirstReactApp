15f3d8466a8674b11753386e7033a220a5fcc840
Julie Yudowitch <JulieYudo@gmail.com>
2021-12-14 19:47:22 -0500

    shop api

--- src/pages/Shop/Shop.js
+++ src/pages/Shop/Shop.js
@@ -6,16 +6,25 @@ import axios from 'axios';
 
 function Shop() {
     const [showShopMenu, setShowShopMenu] = useState(false);
-    const [result, setResult] = useState([])
+    const [result, setResult] = useState([]);
     const toggleShopMenu = () => setShowShopMenu(!showShopMenu);
-    
+
+    useEffect(() => {
+      fakestore();
+    }, [])
+
+   const fakestore = async () => {
+        const response = await fetch('https://fakestoreapi.com/products');
+       const jsonData = await response.json();
+       setResult(jsonData);
+    }    
 
     return (
         <div className='shop-page'>
             <div className='shop-poster'>
                 <FcMenu className='shop-menu-icon' onClick={toggleShopMenu} />
                 <div className='shop-sidebar' >                    
-                    <ul id={showShopMenu ? 'hidden' : 'shown'} onClick={toggleShopMenu}>
+                    <ul id={!showShopMenu ? 'hidden' : 'shown'} onClick={toggleShopMenu}>
                         <li>Women's</li>
                         <li>Men's</li>
                         <li>Children's</li>
@@ -26,7 +35,13 @@ function Shop() {
                     </ul>
                 </div>
                 <div className='products'>
-                  
+                    {result.map((values) => {
+                        return (
+                          <div>
+                                <h5>{values.title}</h5>   
+                          </div>
+                      )
+                  })}
                 </div>
             </div>
             
